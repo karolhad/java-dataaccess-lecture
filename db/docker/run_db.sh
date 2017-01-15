c10e3f926259e417fcd7d1bfb04ec10399cb60e6
Karol Hadala <karol.hadala@oracle.com>
2017-01-15 21:33:33 +0100

    update docker bash script

--- db/docker/run_db.sh
+++ db/docker/run_db.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-#docker run --name bookstore-postgres -e POSTGRES_PASSWORD=password1 -e POSTGRES_DB=bookstore -d postgres 
-docker run --name bookstore-postgres -d bookstore_postgres 
+docker run --rm --name bookstore-postgres -p 5432:5432 bookstore_postgres
