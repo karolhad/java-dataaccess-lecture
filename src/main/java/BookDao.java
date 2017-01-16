1a7081a7aaba21273d96286aa3defa15837447ef
Karol Hadala <karol.hadala@ig.com>
2017-01-16 21:04:23 +0100

    add book dao implementation

--- src/main/java/BookDao.java
+++ src/main/java/BookDao.java
@@ -1,5 +1,6 @@
+import dto.Author;
 import dto.Book;
-import dto.Customer;
+import dto.Subject;
 
 import java.sql.*;
 import java.util.LinkedList;
@@ -8,79 +9,78 @@ import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import static java.util.Arrays.stream;
-
-/**
- * Created by karol on 14/01/2017.
- */
 public class BookDao {
 
-    List<Book> find(String title, String authorLastName, String subject) throws SQLException {
+   List<Book> find(String title, String authorLastName, String subject) throws SQLException {
 
-        try (Connection connection = createConnection();
-             PreparedStatement statement = connection.prepareStatement(prepareQuery(title, authorLastName, subject))
-        ) {
-            setParams(statement, title, authorLastName, subject);
+      try (Connection connection = createConnection();
+           PreparedStatement statement = connection.prepareStatement(prepareQuery(title, authorLastName, subject))
+      ) {
+         setParams(statement, title, authorLastName, subject);
 
-            try (ResultSet resultSet = statement.executeQuery()) {
+         try (ResultSet rs = statement.executeQuery()) {
 
-                List<Book> results = new LinkedList<>();
+            List<Book> results = new LinkedList<>();
 
-                Integer currentBookId = null;
-                Book currentBook;
-                while (resultSet.next()) {
-                    Integer bookId = resultSet.getInt("books.id");
-                    if (!bookId.equals(currentBookId)) {
-                        currentBook = new
-                        results.add(book);
-                    }
+            while (rs.next()) {
+               results.add(new Book(rs.getInt("book_id"),
+                           rs.getString("title"),
+                           new Subject(
+                                 rs.getInt("subject_id"),
+                                 rs.getString("subject")
+                           ),
+                           new Author(
+                                 rs.getInt("author_id"),
+                                 rs.getString("first_name"),
+                                 rs.getString("last_name")
+                           )
+                     )
+               );
 
-
-
-                }
-                return results;
             }
-        }
-    }
+            return results;
+         }
+      }
+   }
 
-    private void setParams(PreparedStatement statement, String title, String authorLastName, String subject) throws SQLException {
-        int paramIndex = 1;
-        if (title != null) {
-            statement.setString(paramIndex++, title);
-        }
-        if (authorLastName != null) {
-            statement.setString(paramIndex++, authorLastName);
-        }
-        if (subject != null) {
-            statement.setString(paramIndex, subject);
-        }
-    }
+   private void setParams(PreparedStatement statement, String title, String authorLastName, String subject) throws SQLException {
+      int paramIndex = 1;
+      if (title != null) {
+         statement.setString(paramIndex++, title);
+      }
+      if (authorLastName != null) {
+         statement.setString(paramIndex++, authorLastName);
+      }
+      if (subject != null) {
+         statement.setString(paramIndex, subject);
+      }
+   }
 
-    private String prepareQuery(String title, String authorLastName, String subject) {
-        String sql = "SELECT * FROM books " +
-                "INNER JOIN authors ON books.author_id = authors.id " +
-                "INNER JOIN subjects ON books.subject_id = subjects.id ";
+   private String prepareQuery(String title, String authorLastName, String subject) {
+      String sql = "SELECT books.id as book_id, title, authors.id as author_id, first_name, last_name, subjects.id as subject_id, subject FROM books " +
+            "INNER JOIN authors ON books.author_id = authors.id " +
+            "INNER JOIN subjects ON books.subject_id = subjects.id ";
 
-        List<String> whereConditions = Stream.of(
-                addParamToQuery("title", title),
-                addParamToQuery("authorLastName", authorLastName),
-                addParamToQuery("subject", subject)).
-                filter(Objects::nonNull).collect(Collectors.toList());
+      List<String> whereConditions = Stream.of(
+            addParamToQuery("title", title),
+            addParamToQuery("last_name", authorLastName),
+            addParamToQuery("subject", subject)).
+            filter(Objects::nonNull).collect(Collectors.toList());
 
-        if (!whereConditions.isEmpty()) {
-            sql += "WHERE " + String.join(" AND ", whereConditions);
-        }
+      if (!whereConditions.isEmpty()) {
+         sql += "WHERE " + String.join(" AND ", whereConditions);
+      }
 
-        return sql + " ORDER BY books.id";
-    }
+      return sql + " ORDER BY books.id";
+   }
 
-    private String addParamToQuery(String fieldName, String value) {
-        return value != null ? fieldName + "= ?": null;
-    }
+   private String addParamToQuery(String fieldName, String value) {
+      return value != null ? fieldName + "= ?" : null;
+   }
 
-    private Connection createConnection() throws SQLException {
-        return DriverManager.getConnection(
-                "jdbc:postgresql://localhost:5432/bookstore",
-                "postgres", "password1");
-    }
+   private Connection createConnection() throws SQLException {
+      return DriverManager.getConnection(
+            "jdbc:postgresql://localhost:5432/bookstore",
+            "postgres", "password1");
+   }
 }
--- src/test/java/BookDaoTest.java
+++ src/test/java/BookDaoTest.java
@@ -1,18 +1,50 @@
+import dto.Book;
 import org.junit.Test;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Created by karol on 14/01/2017.
  */
 public class BookDaoTest {
 
-@Test
-    public void allBooks() {
+   @Test
+   public void allBooks() throws SQLException {
+      BookDao bookDao = new BookDao();
+      final List<Book> books = bookDao.find(null, null, null);
+
+      assertThat(books).hasSize(13);
+   }
+
+   @Test
+   public void frankHerbertBooks() throws SQLException {
+      BookDao bookDao = new BookDao();
+      final List<Book> books = bookDao.find(null, "Herbert", null);
+
+      assertThat(books).hasSize(1);
+      final Book book = books.get(0);
+      assertThat(book.getAuthor().getLastName()).isEqualTo("Herbert");
+      assertThat(book.getTitle()).isEqualTo("Dune");
+      assertThat(book.getSubject().getSubject()).isEqualTo("Science Fiction");
+   }
+
+   @Test
+   public void complexSearch() throws SQLException {
+      BookDao bookDao = new BookDao();
+      final List<Book> books = bookDao.find("Learning Python", "Lutz", "Computers");
+
+      assertThat(books).hasSize(1);
+      final Book book = books.get(0);
+      assertThat(book.getAuthor().getLastName()).isEqualTo("Lutz");
+      assertThat(book.getTitle()).isEqualTo("Learning Python");
+      assertThat(book.getSubject().getSubject()).isEqualTo("Computers");
+   }
 
 
-
-
-    }
 }
 
 
-}
+
