b3d606ba44adff95522653f84ae2bd80d9f2455c
Karol Hadala <karol.hadala@ig.com>
2017-01-23 09:41:17 +0100

    use external docker image name

--- db/docker/connect_db.bat
+++ db/docker/connect_db.bat
@@ -1,2 +1,1 @@
-#!/bin/bash
 docker run -it --rm --link dealing-postgres:postgres postgres psql -h postgres -U postgres dealing
---
+++ db/docker/connect_host.bat
@@ -0,0 +1,1 @@
+docker run -it --rm --link dealing-postgres:postgres postgres /bin/bash
